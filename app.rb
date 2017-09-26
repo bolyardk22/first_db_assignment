62d699d35f093b5d7b8fce97c116768ee0de8289
bolyardk22 <collinsk22@mycu.concord.edu>
2017-09-26 15:41:10 -0400

    heroku pls work

--- app.rb
+++ app.rb
@@ -40,7 +40,7 @@ end
 
 get '/edit_select' do
 	phonebook = db.exec("Select * From phonebook")
-	erb :edit_select
+	erb :edit_select, locals: {phonebook: phonebook}
 end
 
 post '/edit' do
@@ -52,12 +52,27 @@ get '/edit_info' do
 	phonebook = db.exec("Select * From phonebook")
 	edit_num = params[:edit_num]
 	user_row = db.exec("SELECT * FROM phonebook WHERE phone_number = '#{edit_num}'")
-	erb :edit_page
+	erb :edit_page, locals: {edit_num: edit_num, user_row: user_row, phonebook: phonebook}
 end
 
-post '/delete' do
+post '/edited' do
+	edit_num = params[:edit_num]
 	phonebook = db.exec("Select * From phonebook")
-    deleted = params[:user_delete]    
-    db.exec("DELETE FROM phonebook WHERE phone_number = '#{deleted}'");
+	newfirst_name = params[:newfirst_name]
+	newlast_name = params[:newlast_name]
+	newstreet_address = params[:newstreet_address]
+	newcity = params[:newcity]
+	newstate = params[:newstate]
+	newzip = params[:newzip]
+	newphone_number = params[:newphone_number]
+	newemail_address = params[:newemail_address]
+	db.exec("UPDATE phonebook SET first_name = '#{newfirst_name}', last_name = '#{newlast_name}', street_address = '#{newstreet_address}', city = '#{newcity}', state = '#{newstate}', zip = '#{newzip}', phone_number = '#{newphone_number}', email_address = '#{newemail_address}' WHERE phone_number = '#{edit_num}'")
+	redirect '/phone_book'
+end
+
+post '/deleted' do
+	phonebook = db.exec("Select * From phonebook")
+    edit_num = params[:edit_num]  
+    db.exec("DELETE FROM phonebook WHERE phone_number = '#{edit_num}'");
     redirect '/phone_book'
 end
