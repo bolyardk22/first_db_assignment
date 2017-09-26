f38dfe44e9f6bb6cc34a5ca98cdab4fb3ab9d044
bolyardk22 <collinsk22@mycu.concord.edu>
2017-09-26 09:48:48 -0400

    recent push

--- app.rb
+++ app.rb
@@ -34,5 +34,30 @@ get '/success' do
 end
 
 get '/phone_book' do
-	erb :phone_book
+	phonebook = db.exec("Select * From phonebook")
+	erb :phone_book, locals: {phonebook: phonebook}
+end
+
+get '/edit_select' do
+	phonebook = db.exec("Select * From phonebook")
+	erb :edit_select
+end
+
+post '/edit' do
+	edit_num = params[:edit_select_table_radio]
+	redirect '/edit_info?edit_num=' + edit_num
+end
+
+get '/edit_info' do
+	phonebook = db.exec("Select * From phonebook")
+	edit_num = params[:edit_num]
+	user_row = db.exec("SELECT * FROM phonebook WHERE phone_number = '#{edit_num}'")
+	erb :edit_page
+end
+
+post '/delete' do
+	phonebook = db.exec("Select * From phonebook")
+    deleted = params[:user_delete]    
+    db.exec("DELETE FROM phonebook WHERE phone_number = '#{deleted}'");
+    redirect '/phone_book'
 end
